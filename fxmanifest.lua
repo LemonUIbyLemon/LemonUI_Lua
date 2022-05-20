72378c90406fc15600c12e1d78c5be385dd1e82a
Hannele Ruiz <justlemoncl@gmail.com>
2022-05-19 21:54:54 -0400

    Added main manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "cerulean"
+games { "gta5" }
+
+author "Hannele 'Lemon' Ruiz"
+description "UI Framework for Grand Theft Auto V"
+version "1.0"
+
+lua54 "yes"
+dependency "/gameBuild:2060"
